c9d0ef21ab83464b3a92f1e434cba76d186af259
FaustVII7 <vaslykar@yandex.ru>
2023-06-24 21:14:11 +0300

    Добавьте файлы проекта.

---
+++ unit9_1/Program.cs
@@ -0,0 +1,34 @@
+﻿public class MyCustomException : Exception
+{
+    public MyCustomException(string message) : base(message) { }
+}
+
+try
+{
+}
+catch (Exception e)
+{
+    if (e is MyCustomException)
+    {
+        Console.WriteLine("My Custom Exception Occurred: " + e.Message);
+    }
+    else if (e is InvalidOperationException)
+    {
+        Console.WriteLine("Invalid Operation Exception Occurred: " + e.Message);
+    }
+    else if (e is NullReferenceException)
+    {
+        Console.WriteLine("Null Reference Exception Occurred: " + e.Message);
+    }
+    else if (e is DivideByZeroException)
+    {
+        Console.WriteLine("Divide By Zero Exception Occurred: " + e.Message);
+    }
+    else if (e is ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("Argument Out Of Range Exception Occurred: " + e.Message);
+    }
+}
+finally
+{
+}
